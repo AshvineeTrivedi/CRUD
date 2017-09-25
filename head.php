fb7590bb9b84a134332bdbc70d474a66aff62d76
AshvineeTrivedi <trivediashvinee14@gmail.com>
2017-09-25 16:30:14 +0530

    upload folder

---
+++ head.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <title>Admin Home Page</title>
+        <!-- Bootstrap -->
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+        <link href="vendors/easypiechart/jquery.easy-pie-chart.css" rel="stylesheet" media="screen">
+        <link href="assets/styles.css" rel="stylesheet" media="screen">
+        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <script src="vendors/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    </head>
+	<body>
+        <div class="navbar navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container-fluid">
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span>
+                     <span class="icon-bar"></span>
+                     <span class="icon-bar"></span>
+                    </a>
+                    <a class="brand">Admin </a>
+                    <div class="nav-collapse collapse">
+                    <ul class="nav">
+						<li class="active">
+							<a href="index.php">Home</a>
+                        </li>
+						<li class="active">
+							<a href="reg.php">Registration</a>
+                        </li>
+						<li class="active">
+							<a href="view_stud.php">Student</a>
+                        </li>
+					</ul>
+					</div>
+					</div>
+					</div>
+					</div>
